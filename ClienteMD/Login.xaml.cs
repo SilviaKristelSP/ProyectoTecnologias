a7bd18cac4a37892ec0b0005124568f800e96554
Marlon MP <mrmarlonmp@gmail.com>
2022-06-11 17:19:26 -0500

    GUI de Login creada

---
+++ ClienteMD/Login.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+namespace ClienteMD
+{
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void moverVentana(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
+
+        private void clickSalir(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clickIngresar(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine($"Usuario: {Usuario.Text} Contraseña:{Contraseña.Password}");
+        }
+
+        private void clickRegistrarse(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
