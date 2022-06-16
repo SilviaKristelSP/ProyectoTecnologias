6982f6b59ef28bfad26ce7e9bc61d8696f34207d
Marlon MP <mrmarlonmp@gmail.com>
2022-06-16 16:34:36 -0500

    Creacion de GUI Registro de usuario

--- ClienteMD/Login.xaml.cs
+++ ClienteMD/Login.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClienteMD.Vistas;
+using System;
 using System.Windows;
 using System.Windows.Input;
 namespace ClienteMD
@@ -30,7 +31,9 @@ namespace ClienteMD
 
         private void clickRegistrarse(object sender, RoutedEventArgs e)
         {
-
+            RegistroUsuario registroUsuario = new RegistroUsuario();
+            this.Close();
+            registroUsuario.Show();
         }
     }
 }
---
+++ ClienteMD/Vistas/RegistroUsuario.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ClienteMD.Vistas
+{
+    /// <summary>
+    /// Interaction logic for RegistroUsuario.xaml
+    /// </summary>
+    public partial class RegistroUsuario : Window
+    {
+        public RegistroUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void clickSalir(object sender, RoutedEventArgs e)
+        {
+            Login login = new Login();
+            login.Show();
+            this.Close();
+        }
+
+        private void moverVentana(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
+
+        private void clickRegistrar(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Boton registra");
+        }
+    }
+}
